2b6b9d262695059b5816c12b8d29fda8edd49fe0
Raja Ali <raja@codeworx.com.au>
2017-08-23 11:26:32 +1000

    working with propTypes and prop, iteration, object passing from other js

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,17 +1,44 @@
 import React from "react";
+import PropTypes from 'prop-types'; //to make sure the object type we want to use for, we specify using Prop
 
 export class Home extends React.Component {
     render() {
-        return (
+    	var sometext = "This is a variable declared";
+    	
+    	// logging the properties
+    	console.log(this.props);
+        
+    	return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-12 col-xs-offset-1">
                         <h1>1+1 = {1+1} is a Home component.!</h1>
+                        <p>Retrieving from property</p>
+                       
+                       <p>Your name : {this.props.name}</p>
+                       <p>Your age : {this.props.age}</p>
+                         <p>Declared variable: <b>{sometext}</b></p>
                         
+                         <p>User Object => Name: {this.props.user.name}</p>
+                        
+                         <ul>
+                         	{this.props.user.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>)}
+                         </ul>
                     </div>
+                    <hr/>
+                    {this.props.children}
                 </div>
             </div>
         );
 
     }
 }
+
+
+Home.propTypes = {
+		name: PropTypes.string,
+		age: PropTypes.number,
+		user: PropTypes.object,
+		children: PropTypes.element.isRequired
+};
+
--- src/app/index.js
+++ src/app/index.js
@@ -8,6 +8,13 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
     render() {
+    	
+    	//Creating a user object
+    	var user = {
+    		name: "Anna",
+    		hobbies: ["Sports", "Movies", "Reading"] 
+    	}
+    	
         return (
             <div className="container">
 
@@ -25,7 +32,9 @@ class App extends React.Component {
 
                 <div className="row">
                     <div className="col-xs-12 col-xs-offset-1">
-                        <Home />
+                        <Home name={"Halen"} age={24} user={user}>
+                        	<p>this is a pragraph passed from another js</p>
+                        </Home>
                     </div>
                 </div>
             </div>
